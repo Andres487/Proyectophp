bdb64a4c776c01605e933fe1a6b4e619a354fc35
AndresPalacios <andres487ap@gmail.com>
2020-09-11 08:50:30 -0500

    Primer commit

---
+++ database/migrations/2020_09_05_223343_create_rentals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRentalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('start_date');
+            $table->date('end_date');
+            //campo "total"
+            $table->integer('user_id');
+            $table->integer('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rentals');
+    }
+}
